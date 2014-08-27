fa736607cd4e2d27f5c0851b7d6c1d2d61897a64
GeekTrainer <charrison@geektrainer.com>
2014-08-27 09:59:51 -0700

    Module 1 - Christopher

---
+++ BootstrapMVADemos/HelloBootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloBootstrap.Startup))]
+namespace HelloBootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
