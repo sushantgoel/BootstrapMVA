dbf8ed51160bb669df2cb194ae1e26eeeb980abf
Jon Galloway <jongalloway@gmail.com>
2014-08-27 11:31:24 -0700

    Added Module 2 code

---
+++ Module 2 - Components/Bootstrap Components/Bootstrap Components/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap_Components.Startup))]
+namespace Bootstrap_Components
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
