14ba32579b9d4bfc9b9a27f15b54ae6f860b657e
GeekTrainer <charrison@geektrainer.com>
2014-08-27 16:56:30 -0700

    Last of the day

---
+++ BootstrapMVADemos/MvcDemo/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.Models
+{
+	public class Album
+	{
+		public int AlbumID { get; set; }
+		public String Title { get; set; }
+		public String Artist { get; set; }
+	}
+}
